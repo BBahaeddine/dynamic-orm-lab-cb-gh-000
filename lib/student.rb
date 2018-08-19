8c7ecb0635c564f2d56491978bdabca8ad3fe56e
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-19 14:39:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
   def intialize(args)
     args.each{|key, value|
       self.send("#{key}=", value)
